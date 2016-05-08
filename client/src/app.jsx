28d0c47fa40b5a0f0c504f751dd543365bf3d4a9
Harry Potter <bpeynetti@u.northwestern.edu>
2016-05-08 17:13:32 +0000

    Added graphs and styling for bootstrap.html page

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,11 @@
 var React = require("react");
 
 // var barCharts = require('./components/barCharts.jsx');
-var hBarChart = require('./components/horizontalBarChart.jsx');
-var BarChart = require('./components/barChart.jsx');
+// var hBarChart = require('./components/horizontalBarChart.jsx');
+// var BarChart = require('./components/barChart.jsx');
 var HorizBarChart = require('./components/horizBarChart.jsx');
 var CategoryPicker = require('./components/categoryPicker.jsx');
+var MainPage = require('./components/main_page.jsx');
 
 var ReactRouter = require('react-router');
 var ReactDom = require('react-dom');
@@ -17,6 +18,7 @@ var App = React.createClass({
     return (
 
       <div className="container">
+        <MainPage></MainPage>
         <CategoryPicker></CategoryPicker>
         <HorizBarChart></HorizBarChart>
       </div>
