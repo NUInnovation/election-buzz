6ff73cf78439e12ad744ad50b98c9f186bad7077
MichaelGofron <gofronm@yahoo.com>
2016-05-16 20:03:22 -0500

    (refactor) properly indented html on main_page.jsx

--- client/src/components/main_page.jsx
+++ client/src/components/main_page.jsx
@@ -147,9 +147,10 @@ var MainPage = React.createClass({
 
     $('select').on('change', function(){
       theme = this.value;
-      d3.json("../data/tweetdata_followers.json",function(json,error){
-        if (error) return console.warn(error);
-         
+      d3.json("../data/tweetdata_followers.json",function(json){
+        console.log("pre error");
+        // if (error) return console.warn(error);
+        console.log("post error");
         var data = json.Themes.filter(rightTheme);
 
         var data = data[0].Data;
@@ -179,88 +180,85 @@ var MainPage = React.createClass({
 
   render:function(){
     return(
-      <div>
-        <nav className="navbar navbar-inverse">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>                        
-              </button>
-              <a className="navbar-brand" href="#">Election Buzz</a>
-            </div>
-            <div className="collapse navbar-collapse" id="myNavbar">
-              <ul className="nav navbar-nav">
-              </ul>
-            </div>
-          </div>
-        </nav>
-          
-        <div className="container-fluid text-center">    
-          <div className="row content">
-            <div className="col-sm-2" >
-            </div>
-            <div className="col-sm-8 text-center"> 
-              <img src='../images/logo.png' />
-              <h3> % of tweets related to: </h3>
-              <select name="Theme" className="selectForm">
-                <option value="Education">Education</option>
-                <option value="Security">Security</option>
-                <option value="Immigration">Immigration</option>
-                <option value="Women">Women</option>
-                <option value="Healthcare">Healthcare</option>
-                <option value="Supreme Court">Supreme Court</option>
-                <option value="Tax">Taxes</option>
-              </select>
-              <div id='title_graph'>
-                <div className='row'>
-                  <div className="col-sm-6">
-                      <h3>Followers</h3>
-                  </div>
-                  <div className="col-sm-6">
-                      <h3>Candidates (past 3000 tweets)</h3>
-                  </div>
-                </div>
-              </div>
-              
-              <div id="graph" className="row">
-                <div className= "col-sm-5" id="candidatetweets">
-                  <div id="fChart"></div>
-                </div>
-                 <div className= "col-sm-2" id="pictures">
-                  <div className='candidatePicture'>
-                    <a href="candidate.html?name=Hillary">
-                      <img src='images/hillary.jpg' className='img-circle candidateImg' />
-                    @HillaryClinton           
-                    </a>
-                  </div>
-                  <div className='candidatePicture'>
-                    <a href="candidate.html?name=Bernie">
-                      <img src='images/bernie.jpg' className='img-circle candidateImg' />
-                    @BernieSanders
-                    </a>
-                  </div>
-                  <div className='candidatePicture'>  
-                    <a href="candidate.html?name=Trump">
-                      <img src='images/trump.jpg' className='img-circle candidateImg' />
-                    @realDonaldTrump
-                    </a>
-                  </div>
-                </div>
-                <div className= "col-sm-5" id="followertweets">
-                  <div id="chart">
-                </div>
-              </div>
-            </div>
-          </div>
-          <br/>
-          <br/>
-          <br/>
-
-        </div>
+<div>
+  <nav className="navbar navbar-inverse">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="#">Election Buzz</a>
+      </div>
+      <div className="collapse navbar-collapse" id="myNavbar">
+        <ul className="nav navbar-nav">
+        </ul>
       </div>
     </div>
+  </nav>
+
+  <div className="container-fluid text-center">
+    <div className="row content">
+      <div className="col-sm-2">
+      </div>
+      <div className="col-sm-8 text-center">
+        <img src='../images/logo.png' />
+        <h3> % of tweets related to: </h3>
+        <select name="Theme" className="selectForm">
+          <option value="Education">Education</option>
+          <option value="Security">Security</option>
+          <option value="Immigration">Immigration</option>
+          <option value="Women">Women</option>
+          <option value="Healthcare">Healthcare</option>
+          <option value="Supreme Court">Supreme Court</option>
+          <option value="Tax">Taxes</option>
+        </select>
+        <div id='title_graph'>
+          <div className='row'>
+            <div className="col-sm-6">
+              <h3>Followers</h3>
+            </div>
+            <div className="col-sm-6">
+              <h3>Candidates (past 3000 tweets)</h3>
+            </div>
+          </div>
+        </div>
+
+        <div id="graph" className="row">
+          <div className="col-sm-5" id="candidatetweets">
+            <div id="fChart"></div>
+          </div>
+          <div className="col-sm-2" id="pictures">
+            <div className='candidatePicture'>
+              <a href="candidate.html?name=Hillary">
+                <img src='images/hillary.jpg' className='img-circle candidateImg' /> @HillaryClinton
+              </a>
+            </div>
+            <div className='candidatePicture'>
+              <a href="candidate.html?name=Bernie">
+                <img src='images/bernie.jpg' className='img-circle candidateImg' /> @BernieSanders
+              </a>
+            </div>
+            <div className='candidatePicture'>
+              <a href="candidate.html?name=Trump">
+                <img src='images/trump.jpg' className='img-circle candidateImg' /> @realDonaldTrump
+              </a>
+            </div>
+          </div>
+          <div className="col-sm-5" id="followertweets">
+            <div id="chart">
+            </div>
+          </div>
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+
+    </div>
+  </div>
+</div>
       );
   }
   
